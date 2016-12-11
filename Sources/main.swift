a34fd29a77c0fcd522d187e26546d843db5754de
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-12-10 18:50:56 -0800

    Make routes

--- Sources/main.swift
+++ Sources/main.swift
@@ -5,8 +5,19 @@ import HeliumLogger
 HeliumLogger.use()
 let router = Router()
 
-router.all("/") { request, response, next in
-  response.send("Hello, Kitura!")
+router.get("/") { request, response, next in
+  response.send("Welcome to East Valley Pet Hospital")
+  next()
+}
+
+router.get("/staff") { request, response, next in
+  response.send("Meet our great team")
+  next()
+}
+
+router.get("/contact") { request, response, next in
+  response.send("Get in touch with us")
+  next()
 }
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
