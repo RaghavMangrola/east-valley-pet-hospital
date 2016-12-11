debe291332ea5a79fa6a88789236b7ceb84b0ed4
Raghav Mangrola <raghav.mangrola@gmail.com>
2016-12-11 13:48:48 -0800

    Custom routes for Staff + Staff Template

--- Sources/main.swift
+++ Sources/main.swift
@@ -12,9 +12,29 @@ router.get("/") { request, response, next in
   try response.render("home", context: [:])
 }
 
-router.get("/staff") { request, response, next in
-  response.send("Meet our great team")
-  next()
+router.get("/staff/:name") { request, response, next in
+  defer { next() }
+
+  guard let name = request.parameters["name"] else {
+    return
+  }
+
+  let bios = [
+    "raghav": "My name is Raghav Mangrola and I love all dogs.",
+    "parth": "My name is Parth and I don't like dogs very much",
+    "maya": "My name is Maya and I'm a crazy cat lady",
+    "harisinh": "My name is Harisinh and I love all animals"
+  ]
+
+  var context = [String: Any]()
+  context["people"] = bios.keys.sorted()
+
+  if let bio = bios[name] {
+    context["name"] = name
+    context["bio"] = bio
+  }
+
+  try response.render("staff", context: context)
 }
 
 router.get("/contact") { request, response, next in
